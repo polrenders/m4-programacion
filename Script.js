bb8c975313624e869d9a82370e1aa33c23ce0267
polrenders <polrenders@gmail.com>
2025-04-07 23:05:13 +0200

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,4 +1,7 @@
 // script.js
+
+
+
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('nav');
     // const heroSection = document.querySelector('.hero');
@@ -10,3 +13,33 @@ window.addEventListener('scroll', function () {
         navbar.style.opacity = '1';
     }
 });
+
+
+    const form = document.getElementById('contactForm');
+    const mensajeEnviado = document.getElementById('mensajeEnviado');
+    const volverBtn = document.getElementById('volverBtn');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+    const nombre = document.getElementById('nombre').value.trim();
+    const correo = document.getElementById('correo').value.trim();
+    const interes = document.getElementById('interes').value;
+    const mensaje = document.getElementById('mensaje').value.trim();
+    const terminos = document.getElementById('terminos').checked;
+
+    if (!nombre || !correo || !interes || !mensaje || !terminos) {
+        alert('Por favor, completa todos los campos y acepta los términos.');
+    return;
+    }
+
+    // Simula envío
+    form.classList.add('hidden');
+    mensajeEnviado.classList.remove('hidden');
+    form.reset();
+  });
+
+  volverBtn.addEventListener('click', () => {
+        mensajeEnviado.classList.add('hidden');
+    form.classList.remove('hidden');
+  });
